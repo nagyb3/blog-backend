0caacd5901d482fde079f5666b84897427349d25
Bence Nagy <bence@veldex.at>
2025-03-08 13:25:32 +0100

    idk

--- src/main/java/org/example/blogbackend/BlogBackendApplication.java
+++ src/main/java/org/example/blogbackend/BlogBackendApplication.java
@@ -7,9 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogBackendApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Debug: MYSQL_URL: " + System.getenv("MYSQL_URL") );
 		System.out.println("Debug: MYSQL_PASSWORD: " + System.getenv("MYSQL_PASSWORD") );
 		System.out.println("Debug: MYSQL_USER: " + System.getenv("MYSQL_USER") );
+		System.out.println("Debug: MYSQL_HOST: " + System.getenv("MYSQL_HOST") );
+		System.out.println("Debug: MYSQL_PORT: " + System.getenv("MYSQL_PORT") );
+		System.out.println("Debug: MYSQL_DATABASE: " + System.getenv("MYSQL_DATABASE") );
 
 		SpringApplication.run(BlogBackendApplication.class, args);
 	}
